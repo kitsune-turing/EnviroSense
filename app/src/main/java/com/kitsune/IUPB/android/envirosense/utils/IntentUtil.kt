60c77c59dfd2927eb338cba58c863cd82687fe43
= <=>
2024-11-10 19:56:58 -0500

    Add util for intents

--- app/src/main/java/com/kitsune/IUPB/android/envirosense/utils/IntentUtil.kt
+++ app/src/main/java/com/kitsune/IUPB/android/envirosense/utils/IntentUtil.kt
@@ -3,12 +3,14 @@ package com.kitsune.IUPB.android.envirosense.utils
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 
 object IntentUtil {
     // Método único para navegar a cualquier actividad
     fun navigateToActivity(context: Context, activityClass: Class<*>) {
         val intent = Intent(context, activityClass)
+        Log.d("IntentUtil", "Open new activity")
         context.startActivity(intent)
     }
 }
